4f6fbf61f3320890f8cb53bf96f33eae217b31a2
Görkem Sever <gorkem_se_98@hotmail.com>
2023-08-15 06:13:56 -0400

    Cleaning some shits.

--- backups/2023-07-04_2-gorkem/srcs/requirements/nginx/studying/create_port_range.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in {7000..7010}; do
-    docker run -d -v /home/gorkem/Desktop/inception/srcs/requirements/nginx/studying/:/usr/share/nginx/html --name test-nginx-port_$i -p $i:80 nginx
-done
-
